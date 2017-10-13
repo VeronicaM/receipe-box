65dd662b87a81e0cd10987958ef7b6247bab8433
Mihai <mihaiveronica@ymail.com>
2017-10-13 20:46:46 +0300

    added react and sass setup with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/*
+./webpack.config.js
+*/
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: ['./client/index.js', './client/app.scss'],
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!autoprefixer-loader!sass-loader"
+                })
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|ttf)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: { limit: 10000 } // Convert images < 10k to base64 strings
+                }]
+            }
+        ]
+
+    },
+    plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin({
+        filename: 'dist/style.css',
+        allChunks: true,
+    })]
+}
